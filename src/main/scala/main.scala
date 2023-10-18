d93b107a6a3a05ebdc455dc2e310d7ca01977441
Sisir Koppaka <sisir.koppaka@gmail.com>
2023-10-18 20:55:39 +0530

    implement gpt 3.5 turbo chat completions as standard for structured data

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -7,70 +7,120 @@ import io.cequence.openaiscala.service.*
 import io.cequence.openaiscala.domain.response.*
 import io.cequence.openaiscala.domain.ModelId
 import com.typesafe.config.*
-
+import io.cequence.openaiscala.domain.settings.CreateCompletionSettings
+import io.cequence.openaiscala.domain.{ChatRole, MessageSpec}
+import io.cequence.openaiscala.domain.settings.CreateChatCompletionSettings
 import scala.concurrent.*
 import scala.concurrent.duration.Duration
-import scalatags.Text.all._
 import io.undertow.Undertow
-import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
-import upickle.default.*
+import io.circe.*
+import io.circe.generic.auto.*
+import io.circe.parser.*
+import io.circe.syntax.*
+import io.circe.KeyDecoder.decodeKeyString
+import upickle.default._
 
-object Bookstore {
-  case class Book(title: String, author: Author) derives ReadWriter
-  case class Author(name: String, country: String) derives ReadWriter
+object TicketStore {
+  case class Ticket(message: String, author: String) derives ReadWriter
+  case class EnrichedTicket(ticket: Ticket,
+                            product: Option[String],
+                            subject: Option[String],
+                            sentiment: Option[String],
+                            tags:Option[List[String]]) derives ReadWriter
 
 }
-//object BookstorePlayground {
-//  @main
-//  def main(): Unit = {
-//    implicit val ec = ExecutionContext.global
-//    implicit val materializer = Materializer(ActorSystem())
-//
-//    val config = ConfigFactory.load("openai-scala-client.conf")
-//    val service = OpenAIServiceFactory(config)
-//
-//    //  service.listModels.map(models =>
-//    //    models.foreach(println)
-//    //  )
-//
-//    //  val models: Future[Seq[ModelInfo]] = service.listModels
-//
-//    //  models.map(modelSeq => modelSeq.filter(_.owned_by=="openai").foreach(println))
-//
-//    //  service.retrieveModel(ModelId.text_davinci_003).map(model =>
-//    //    println(model.getOrElse("N/A"))
-//    //  )
-//
-//    val text =
-//      """Extract the name and mailing address as a JSON with the fields "name" and "mailing_address" from this email:
-//        |Dear Kelly,
-//        |It was great to talk to you at the seminar. I thought Jane's talk was quite good.
-//        |Thank you for the book. Here's my address 2111 Ash Lane, Crestview CA 92002
-//        |Best,
-//        |Maya
-//             """.stripMargin
-//
-//    println("executing completion")
-//
-//    Await.result(service.createCompletion(text).map(completion =>
-//      println(completion.choices.head.text)
-//    ), Duration.Inf)
-//
-//
-//    service.close()
-//
-//    println("Hello world!")
-//  }
-//}
+object TicketAI {
+  def structuredData(ticket: TicketStore.Ticket): TicketStore.EnrichedTicket = {
+    implicit val ec = ExecutionContext.global
+    implicit val materializer = Materializer(ActorSystem())
 
-object BookStorePlayground extends cask.MainRoutes {
+    val config = ConfigFactory.load("openai-scala-client.conf")
+    val service = OpenAIServiceFactory(config)
+
+    //  service.listModels.map(models =>
+    //    models.foreach(println)
+    //  )
+
+    //  val models: Future[Seq[ModelInfo]] = service.listModels
+
+    //  models.map(modelSeq => modelSeq.filter(_.owned_by=="openai").foreach(println))
+
+    //  service.retrieveModel(ModelId.text_davinci_003).map(model =>
+    //    println(model.getOrElse("N/A"))
+    //  )
+
+    val text =
+      """Extract the name of the product being talked about. Be as specific as possible, and include the brand name and the product name. Reply with just the name as text.
+        | I purchased this mixer from KitchenAid a few weeks ago and found it ridiculously useful to juice up my day.
+        |""".stripMargin
+
+    val textJSON =
+      """Extract the name of the product being talked about. Be as specific as possible, and include the brand name and the product name. Reply in JSON with the field name "name" from this customer service email:
+        | I purchased this mixer from KitchenAid a few weeks ago and found it ridiculously useful to juice up my day.
+        |""".stripMargin
+
+
+    println(text)
+
+//    val result = Await.result(service.createCompletion(ticket.message,
+//      settings = CreateCompletionSettings(model = ModelId.gpt_3_5_turbo)
+//    ).map(completion =>
+//      (completion.choices.head.text)
+//    ), Duration.Inf)
+
+    val createChatCompletionSettings = CreateChatCompletionSettings(
+      model = ModelId.gpt_3_5_turbo
+    )
+
+    val messages: Seq[MessageSpec] = Seq(
+      MessageSpec(role = ChatRole.System, content = "You are a helpful assistant."),
+//      MessageSpec(role = ChatRole.User, content = "Who won the world series in 2020?"),
+//      MessageSpec(role = ChatRole.Assistant, content = "The Los Angeles Dodgers won the World Series in 2020."),
+      MessageSpec(role = ChatRole.User, content = text),
+    )
+
+    val result = Await.result(service.createChatCompletion(
+      messages = messages,
+      settings = createChatCompletionSettings
+    ).map { chatCompletion =>
+      chatCompletion.choices.head.message.content
+    }, Duration.Inf)
+
+    service.close()
+
+    TicketStore.EnrichedTicket(ticket = ticket,
+      product = Some(result),
+      subject = None,
+      sentiment = None,
+      tags = None
+    )
+  }
+}
+
+object TicketsPlayground extends cask.MainRoutes {
   override def host: String = "0.0.0.0"
 
   override def port: Int = 80
 
+  val originalTicket = TicketStore.Ticket(
+    message = "I purchased this mixer from KitchenAid a few weeks ago and found it ridiculously useful to juice up my day",
+    author = "Sisir Koppaka")
+
   @cask.get("/")
   def hello() =
-    upickle.default.write(Bookstore.Book("Pursuit of Happyness", Bookstore.Author("Will Smith", "USA")))
+    upickle.default.write(originalTicket)
+
+  @cask.post("/read-request")
+  def readRequest(request: cask.Request) = {
+    val ticketRead = upickle.default.read[TicketStore.Ticket](request.text())
+    upickle.default.write(ticketRead.author)
+  }
+
+  @cask.post("/structured-data")
+  def structuredData(request: cask.Request) = {
+    val ticketRead = upickle.default.read[TicketStore.Ticket](request.text())
+    upickle.default.write(TicketAI.structuredData(ticketRead))
+  }
 
   initialize()
 }
