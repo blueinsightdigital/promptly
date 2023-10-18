c7ae8b55b86cb237960773d30b91f96794137e31
Sisir Koppaka <sisir.koppaka@gmail.com>
2023-10-18 21:51:35 +0530

    add additional apis for determine-sentiment and set-tags

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -30,7 +30,7 @@ object TicketStore {
 
 }
 object TicketAI {
-  def structuredData(ticket: TicketStore.Ticket): TicketStore.EnrichedTicket = {
+  def structuredData(ticket: TicketStore.Ticket, prompt: String): TicketStore.EnrichedTicket = {
     implicit val ec = ExecutionContext.global
     implicit val materializer = Materializer(ActorSystem())
 
@@ -48,20 +48,7 @@ object TicketAI {
     //  service.retrieveModel(ModelId.text_davinci_003).map(model =>
     //    println(model.getOrElse("N/A"))
     //  )
-
-    val text =
-      """Extract the name of the product being talked about. Be as specific as possible, and include the brand name and the product name. Reply with just the name as text.
-        | I purchased this mixer from KitchenAid a few weeks ago and found it ridiculously useful to juice up my day.
-        |""".stripMargin
-
-    val textJSON =
-      """Extract the name of the product being talked about. Be as specific as possible, and include the brand name and the product name. Reply in JSON with the field name "name" from this customer service email:
-        | I purchased this mixer from KitchenAid a few weeks ago and found it ridiculously useful to juice up my day.
-        |""".stripMargin
-
-
-    println(text)
-
+    println(prompt)
 //    val result = Await.result(service.createCompletion(ticket.message,
 //      settings = CreateCompletionSettings(model = ModelId.gpt_3_5_turbo)
 //    ).map(completion =>
@@ -76,7 +63,7 @@ object TicketAI {
       MessageSpec(role = ChatRole.System, content = "You are a helpful assistant."),
 //      MessageSpec(role = ChatRole.User, content = "Who won the world series in 2020?"),
 //      MessageSpec(role = ChatRole.Assistant, content = "The Los Angeles Dodgers won the World Series in 2020."),
-      MessageSpec(role = ChatRole.User, content = text),
+      MessageSpec(role = ChatRole.User, content = prompt),
     )
 
     val result = Await.result(service.createChatCompletion(
@@ -119,7 +106,66 @@ object TicketsPlayground extends cask.MainRoutes {
   @cask.post("/structured-data")
   def structuredData(request: cask.Request) = {
     val ticketRead = upickle.default.read[TicketStore.Ticket](request.text())
-    upickle.default.write(TicketAI.structuredData(ticketRead))
+
+    val prompt =
+      """Extract the name of the product being talked about. Be as specific as possible, and include the brand name and the product name. Reply with just the name as text.
+        | I purchased this mixer from KitchenAid a few weeks ago and found it ridiculously useful to juice up my day.
+        |""".stripMargin
+
+    val promptJSON =
+      """Extract the name of the product being talked about. Be as specific as possible, and include the brand name and the product name. Reply in JSON with the field name "name" from this customer service email:
+        | I purchased this mixer from KitchenAid a few weeks ago and found it ridiculously useful to juice up my day.
+        |""".stripMargin
+
+    upickle.default.write(TicketAI.structuredData(ticketRead, prompt = prompt))
+  }
+
+  @cask.post("/summarize-data")
+  def summarizeData(request: cask.Request) = {
+    val ticketRead = upickle.default.read[TicketStore.Ticket](request.text())
+//
+//    val prompt =
+//      """Summarize the review below in a short sentence. Be creative, but be as close to the text below as possible:
+//        | If you're a beginner baker like myself, I highly recommend this artisan model. It's user friendly, and I like how it came with the three different attachments and a pouring shield as well. Also the 5 quart is honestly enough room for an at home baker. However, if you plan to be baking huge batches for a commercial business this one may not be for you and you may want to opt out for a bigger mixer.
+//        |
+//        |""".stripMargin
+
+    val promptEval =
+      """Summarize the review below in a short sentence. Be creative, but be as close to the text below as possible:
+        """+ticketRead.message+"""
+        |
+        |""".stripMargin
+
+
+    upickle.default.write(TicketAI.structuredData(ticketRead, prompt = promptEval))
+  }
+
+  @cask.post("/determine-sentiment")
+  def determineSentiment(request: cask.Request) = {
+    val ticketRead = upickle.default.read[TicketStore.Ticket](request.text())
+
+    val promptEval =
+      """Determine the sentiment of the product review below. Be specific and answer with positive, negative, or neutral. One word response only.
+        """ + ticketRead.message +
+        """
+          |
+          |""".stripMargin
+
+    upickle.default.write(TicketAI.structuredData(ticketRead, prompt = promptEval))
+  }
+
+  @cask.post("/set-tags")
+  def setTags(request: cask.Request) = {
+    val ticketRead = upickle.default.read[TicketStore.Ticket](request.text())
+
+    val promptEval =
+      """Determine upto three tags for the below review. Be specific and descriptive and answer with upto 3 comma-separated one word tags only. Hypenate if requried to ensure each tag is mapped to a single contiguous word.
+        """ + ticketRead.message +
+        """
+          |
+          |""".stripMargin
+
+    upickle.default.write(TicketAI.structuredData(ticketRead, prompt = promptEval))
   }
 
   initialize()
